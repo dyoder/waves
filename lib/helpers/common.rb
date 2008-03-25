1e1d0c373faccd3d955d422c345a89ee9e6a7817
Lawrence Pit <lawrence.pit@gmail.com>
2008-03-25 17:18:53 +1100

    Fixed doctype outputs

--- lib/helpers/common.rb
+++ lib/helpers/common.rb
@@ -7,26 +7,26 @@ module Waves
     module Common
       
       DOCTYPES = {
-		    :html3 => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">',
+		    :html3 => "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 3.2//EN\">\n",
 				:html4_transitional => 
-				  '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"' <<   
-				   '"http://www.w3.org/TR/html4/loose.dtd">',
+				  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" " <<   
+				   "\"http://www.w3.org/TR/html4/loose.dtd\">\n",
 				:html4_strict =>
-					'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" ' <<
-						'"http://www.w3.org/TR/html4/strict.dtd">',
+					"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" " <<
+						"\"http://www.w3.org/TR/html4/strict.dtd\">\n",
 				:html4_frameset =>
-				  '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"' <<
-  				  '"http://www.w3.org/TR/html4/frameset.dtd">',
+				  "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" " <<
+  				  "\"http://www.w3.org/TR/html4/frameset.dtd\">\n",
 				:xhtml1_transitional =>
-				  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"' <<
-				    '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+				  "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" " <<
+				    "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n",
 				:xhtml1_strict =>
-				  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"' <<
-				    '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+				  "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" " <<
+				    "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n",
 				:xhtml1_frameset =>
-				  '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"' <<
-				    '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">',
-				:xhtml2 => '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">'
+				  "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\" " <<
+				    "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">\n",
+				:xhtml2 => "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n"
 		  }
 		
 		  # Invokes a layout view (i.e., a view from the layouts template directory), using
