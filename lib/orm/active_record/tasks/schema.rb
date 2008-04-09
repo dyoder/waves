9f1df7fd51494546de2f570163a132c652e790ce
Matthew King <automatthew@gmail.com>
2008-04-09 16:33:19 -0500

    implemented AR migrations, but haven't tested yet

--- lib/orm/active_record/tasks/schema.rb
+++ lib/orm/active_record/tasks/schema.rb
@@ -16,10 +16,16 @@ namespace :schema do
 	end
 	
 	desc 'Performs migration from version, to version.'
-	task :migrate do |task|
+	task :migrate => :connect do |task|
 	  raise "Unimplemented.  See #{File.expand_path(__FILE__)}"
+	  version = ENV['version'] ? ENV['version'].to_i : nil
+	  ActiveRecord::Migrator.migrate(Migration.directory, version)
 		# version = ENV['version']; version = version.to_i unless version.nil? 
 		#     Sequel::Migrator.apply( Waves.application.database, Migration.directory , version )
+	end
+	
+	task :connect do
+    Application.database
 	end
 	
 end
