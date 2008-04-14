3094097a18a1bcc323d8cee049ee45c426bd46a4
Dan Yoder <dan@zeraweb.com>
2008-04-14 13:04:14 -0700

    added a few more tests using the foundation approach.

--- verify/mapping/path.rb
+++ verify/mapping/path.rb
@@ -14,8 +14,20 @@ specification "A developer can map requests using the request path." do
 
   specify 'Map the path of a GET request to a lambda.' do
     get('/').body.should == 'This is a simple get rule.'
-    get('/foo').body.should.not == 'This is a simple get rule.'
   end
+
+  specify 'Map the path of a POST request to a lambda.' do
+    post('/').body.should == 'This is a simple post rule.'
+  end
+
+  specify 'Map the path of a PUT request to a lambda.' do
+    put('/').body.should == 'This is a simple put rule.'
+  end
+
+  specify 'Map the path of a DELETE request to a lambda.' do
+    delete('/').body.should == 'This is a simple delete rule.'
+  end
+  
 
 end
 
