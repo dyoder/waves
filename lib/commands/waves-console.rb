a6b497eea7c7c45fc2b654b99e7d5ef1c5130c77
Lawrence Pit <lawrence.pit@gmail.com>
2008-04-14 11:11:13 +1000

    waves-console: irb completion

--- lib/commands/waves-console.rb
+++ lib/commands/waves-console.rb
@@ -2,7 +2,7 @@ require 'waves'
 require 'choice'
 
 Choice.options do
-  header 'Run a waves application server.'
+  header 'Run waves in console mode.'
   header ''
   option :mode do
     short '-c'
@@ -22,4 +22,7 @@ Choice.options do
 end
     
 Waves::Console.load( Choice.choices )
-require 'irb'; IRB.start
+require 'irb'
+require 'irb/completion'
+ARGV.clear # make sure arguments to waves-console don't propagate to irb
+IRB.start
