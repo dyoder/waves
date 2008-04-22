7691a2d88ae6ceec4881e93f0fdb166abe843be2
Matthew King <automatthew@gmail.com>
2008-04-22 09:15:31 -0500

    failing test case for handle

--- verify/mapping/handle.rb
+++ verify/mapping/handle.rb
@@ -1,16 +1,23 @@
 # require 'test_helper' because RubyMate needs help
 require File.join(File.dirname(__FILE__), "..", "helpers")
 
-specification "A developer can handle exceptions using some damn thing" do
+specification "A developer can register exception handlers" do
     
   before do
     mapping.clear
   end
 
-  specify 'Map the path of a GET request to a lambda.' do
+  specify 'The minimal 404 handler in SimpleErrors' do
     r = get('/')
     r.status.should == 404
     r.body.should.be.empty
+  end
+  
+  specify "A custom 404 handler should override the minimal" do
+    handle(Waves::Dispatchers::NotFoundError) { response.status = 404; response.body = "gone baby gone"}
+    r = get('/')
+    r.status.should == 404
+    r.body.should == "gone baby gone"
   end
   
 end
