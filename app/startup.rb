5e12458fc4a6d02016b31c17d11f311f43e81590
Matthew King <automatthew@gmail.com>
2008-05-08 14:52:30 -0500

    handle undefined WAVES in startup.rb

--- app/startup.rb
+++ app/startup.rb
@@ -1,5 +1,9 @@
+unless defined?(WAVES)
+  WAVES = ENV['WAVES']
+end
+
 lambda {
-  waves = ( WAVES || ENV['WAVES'] || File.join(File.dirname(__FILE__), 'waves') )
+  waves = ( WAVES || File.join(File.dirname(__FILE__), 'waves') )
   $:.unshift(File.join( waves, "lib" )) if File.exist? waves
 }.call
 require 'waves'
