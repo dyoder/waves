43bd31afdceaeb667666c3e2fdb882283464562e
Matthew King <automatthew@gmail.com>
2008-05-08 20:42:44 -0500

    better startup.rb handling

--- app/startup.rb
+++ app/startup.rb
@@ -1,9 +1,5 @@
-unless defined?(WAVES)
-  WAVES = ENV['WAVES']
-end
-
 lambda {
-  waves = ( WAVES || File.join(File.dirname(__FILE__), 'waves') )
+  waves = ( ( WAVES if defined? WAVES ) || ENV['WAVES'] || File.join(File.dirname(__FILE__), 'waves') )
   $:.unshift(File.join( waves, "lib" )) if File.exist? waves
 }.call
 require 'waves'
