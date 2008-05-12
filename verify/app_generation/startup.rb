69686c13c2e656cc722fcd02358f32f8ee907f83
Matthew King <automatthew@gmail.com>
2008-05-12 18:25:00 -0500

    genitorifying the app generator; copy and process working

--- verify/app_generation/startup.rb
+++ verify/app_generation/startup.rb
@@ -3,7 +3,7 @@
 require File.join(File.dirname(__FILE__) , "helpers")
 
 
-describe "startup.rb in a generated app, without requiring rubygems" do
+describe "startup.rb in a generated app" do
   before do
     clobber_test_app(AppPath)
     generate_test_app(AppPath)
@@ -12,27 +12,19 @@ describe "startup.rb in a generated app, without requiring rubygems" do
   after do
     clobber_test_app(AppPath)
   end
+  
+  it "should make the scripts executable" do
+    File.executable?(File.join(AppPath, "bin", "waves-console")).should.be.true
+  end
 
-  it "should fail to find waves framework" do
+  it "should fail to find waves framework, if gem is not available" do
     Dir.chdir(AppPath) do
       stdin, stdout, stderr = Open3.popen3("ruby startup.rb")
       stderr.gets.should =~ %r{no such file to load -- waves \(LoadError\)}
     end
   end
 
-end
-
-describe "startup.rb in an app, without gems, but with WAVES defined" do
-  before do
-    clobber_test_app(AppPath)
-    generate_test_app(AppPath)
-  end
-
-  after do
-    clobber_test_app(AppPath)
-  end
-
-  it "should find and use waves framework source" do
+  it "should find and use waves framework source when WAVES is defined" do
     set_framework_path(AppPath)
     Dir.chdir(AppPath) do
       stdin, stdout, stderr = Open3.popen3("ruby startup.rb")
