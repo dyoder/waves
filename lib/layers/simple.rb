8c2bbcc948c287a3bf153abf33b2ef051b3fd79a
Matthew King <automatthew@gmail.com>
2008-05-29 20:46:33 -0500

    simple foundation wasn't extending Mappings

--- lib/layers/simple.rb
+++ lib/layers/simple.rb
@@ -16,6 +16,9 @@ module Waves
             auto_create_class true, Waves::Configurations::Default
             auto_load :Mapping, :directories => [:configurations]
             auto_load true, :directories => [:configurations]
+            auto_eval :Mapping do
+              extend Waves::Mapping
+            end
           end
 
         end
