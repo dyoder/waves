f1260bbf243a115b93ba52d72b079dbde45f4296
Matthew King <automatthew@gmail.com>
2008-06-01 15:55:55 -0500

    removed Verify helpers from console; may need to consider alternative

--- lib/commands/waves-console.rb
+++ lib/commands/waves-console.rb
@@ -15,9 +15,6 @@ end
 
 console = Waves::Console.load( Choice.choices )
 Object.send(:define_method, :waves) { console }
-Object.instance_eval do
-  include Waves::Verify::Helpers::Request
-end
 require 'irb'
 require 'irb/completion'
 ARGV.clear
