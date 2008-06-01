5da4591a64f6199a5e5fd37cd0e10d2efebdb2f9
Matthew King <automatthew@gmail.com>
2008-05-31 21:08:29 -0500

    specs for configuration attributes

---
+++ verify/configurations/attributes.rb
@@ -0,0 +1,37 @@
+# require 'test_helper' because RubyMate needs help
+require File.join(File.dirname(__FILE__) , "helpers")
+
+describe "The attributes of a configuration class" do
+  
+  class Basic < Waves::Configurations::Base; end
+  
+  it "can be declared by developers" do
+    Basic.attribute :smurf
+    Basic.smurf("smurfy") 
+    Basic.smurf.should == "smurfy"
+  end
+  
+  
+  it "must be declared before use" do
+    Basic.should.not.respond_to :gnome
+  end
+  
+end
+
+describe "The default configuration class" do
+  
+  class Default < Waves::Configurations::Default; end
+  
+  it "declares certain attributes necessary to run a Waves app" do
+    %w( host port ports log reloadable database session debug root synchronize? ).each do |attr|
+      Default.should.respond_to attr
+    end
+  end
+  
+  it "sets default values for important attributes" do
+    [ :debug, :synchronize?, :session, :log, :reloadable ].each do |attr|
+      Default[attr].should.not.be.nil
+    end
+  end
+  
+end
