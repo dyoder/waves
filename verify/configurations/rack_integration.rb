cb8bab90a50213278128f0e827972377ac4a015f
Matthew King <automatthew@gmail.com>
2008-05-31 21:20:51 -0500

    specs for rack integration bits in configuration

---
+++ verify/configurations/rack_integration.rb
@@ -0,0 +1,27 @@
+# require 'test_helper' because RubyMate needs help
+require File.join(File.dirname(__FILE__) , "helpers")
+
+describe "The default configuration class" do
+  
+  class Default < Waves::Configurations::Default; end
+  
+  it "can specify a Rack handler" do
+    class Default
+      handler(::Rack::Handler::Mongrel, :Host => '0.0.0.0', :Port => 8080)
+    end
+    Default.handler.should == [ ::Rack::Handler::Mongrel, { :Host => '0.0.0.0', :Port => 8080 } ]
+  end
+  
+  it "provides an accessor for a hash of mime types" do
+    Waves::MimeTypes.should.receive(:[]).with('foo.png').and_return('image/png')
+    Default.mime_types['foo.png'].should == 'image/png'
+  end
+  
+  it "defines the application for use with Rack" do
+    Default.application do
+      "stuff"
+    end
+    Default.application.should.be.a.kind_of Rack::Builder
+  end
+  
+end
