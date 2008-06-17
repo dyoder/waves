5f7f67cb948b304dd8a24eb6a81fdf88af019883
Matthew King <automatthew@gmail.com>
2008-06-16 20:53:42 -0500

    skeletoned some generator tasks for Jxhn Hxltxwxngxr

--- lib/tasks/generate.rb
+++ lib/tasks/generate.rb
@@ -1,15 +1,32 @@
-# namespace :generate do
-#   desc 'Generate a new model'
-#   task :model do |task|
-#     template = File.read :models / 'default.rb'
-#     File.write( :models / ENV['name'] + '.rb',
-#       template.gsub('class Default < Sequel::Model',
-#       "class #{ENV['name'].camel_case} < Sequel::Model(:#{ENV['name'].plural})") )
-#   end
-#   desc 'Generate a new controller'
-#   task :controller do |task|
-#     template = File.read :controllers / 'default.rb'
-#     File.write( :controllers / ENV['name'] + '.rb',
-#       template.gsub('class Default',"class #{ENV['name'].camel_case}") )
-#   end
-# end
+namespace :generate do
+  
+  desc 'Generate a new model'
+  task :model do |task|
+    name = ENV['name']
+    # template = File.read :models / 'default.rb'
+    # File.write( :models / ENV['name'] + '.rb',
+    #   template.gsub('class Default < Sequel::Model',
+    #   "class #{ENV['name'].camel_case} < Sequel::Model(:#{ENV['name'].plural})") )
+  end
+  
+  desc 'Generate a new controller'
+  task :controller do |task|
+    # template = File.read :controllers / 'default.rb'
+    # File.write( :controllers / ENV['name'] + '.rb',
+    #   template.gsub('class Default',"class #{ENV['name'].camel_case}") )
+  end
+  
+  task :view do |task|
+    
+  end
+
+  task :helper do |task|
+    
+  end
+  
+  desc 'generates a model, controller, view, helper, and template dir'
+  task :resource do |task|
+    
+  end
+  
+end
