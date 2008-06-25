e868869a1a2c28bfc431c62c58ce149cbdbdd585
Matthew King <automatthew@gmail.com>
2008-06-25 09:12:05 -0500

    moved String utility methods to a module

--- verify/utilities/hash.rb
+++ verify/utilities/hash.rb
@@ -2,15 +2,15 @@
 require File.join(File.dirname(__FILE__) , "helpers")
 require "#{UTILITIES}/hash"
 
-describe "The monkeypatch to Hash" do
+describe "Waves::Utilities::Hash" do
   
-  it "provides a non-destructive method for converting all hash keys to strings" do
+  it "adds a non-destructive method for converting all hash keys to strings" do
     h = { :a => 1, 'b' => 2, 3 => 3}
     h.stringify_keys.should == { 'a' => 1, 'b' => 2, '3' => 3}
     h.should == { :a => 1, 'b' => 2, 3 => 3}
   end
   
-  it "provides a destructive method for converting hash keys to symbols" do
+  it "adds a destructive method for converting hash keys to symbols" do
     h = { "two" => 2, :three => 3}
     h.symbolize_keys!
     h.should == { :two => 2, :three => 3 }
