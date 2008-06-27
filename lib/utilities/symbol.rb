5730412c65c9ca7731c62b3502372274ee8dcecb
Matthew King <automatthew@gmail.com>
2008-06-27 09:17:19 -0500

    created modules for Symbol and Module monkeypatches

--- lib/utilities/symbol.rb
+++ lib/utilities/symbol.rb
@@ -1,7 +1,15 @@
-class Symbol
-  # Does File.join on self and string, converting self to string before invocation.
-  # See String#/ for more.
-  def / ( string )
-    self.to_s / string
+module Waves
+  module Utilities
+    module Symbol
+      # Does File.join on self and string, converting self to string before invocation.
+      # See String#/ for more.
+      def / ( string )
+        self.to_s / string
+      end
+    end
   end
+end
+
+class Symbol
+  include Waves::Utilities::Symbol
 end
