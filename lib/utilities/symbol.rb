ceae649c2a1f9a48fc8bade3ba54823dffd74c50
Matthew King <automatthew@gmail.com>
2008-06-29 15:10:22 -0500

    revert to monkeypatching Symbol#/, as Sequel defines it in a module

--- lib/utilities/symbol.rb
+++ lib/utilities/symbol.rb
@@ -1,15 +1,10 @@
-module Waves
-  module Utilities
-    module Symbol
-      # Does File.join on self and string, converting self to string before invocation.
-      # See String#/ for more.
-      def / ( string )
-        self.to_s / string
-      end
-    end
+class Symbol
+  # Does File.join on self and string, converting self to string before invocation.
+  # See String#/ for more.  
+  #
+  # Note:  This overrides any definitions done politely in modules, such as
+  # {Sequel::SQL::ComplexExpressionMethods}[http://sequel.rubyforge.org/classes/Sequel/SQL/ComplexExpressionMethods.html]
+  def / ( string )
+    self.to_s / string
   end
-end
-
-class Symbol # :nodoc:
-  include Waves::Utilities::Symbol
 end
