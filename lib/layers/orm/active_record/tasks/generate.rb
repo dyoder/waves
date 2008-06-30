69d4d589f060d4b3e2453c9921086ac52f3eefde
Matthew King <automatthew@gmail.com>
2008-06-30 12:53:18 -0500

    added generate:model task for ActiveRecord layer [#15]

---
+++ lib/layers/orm/active_record/tasks/generate.rb
@@ -0,0 +1,28 @@
+namespace :generate do
+  
+    desc 'Generate an ActiveRecord model, with name=<name>'
+    task :model do |task|
+      name = ENV['name']
+      model_name = name.camel_case
+      raise "Cannot generate Default yet" if model_name == 'Default'
+
+      filename = File.expand_path "models/#{name}.rb"
+      if File.exist?(filename)
+        $stderr.puts "#{filename} already exists" 
+        exit
+      end
+
+      model = <<TEXT
+module #{Waves.application.name}
+  module Models
+    class #{model_name} < Default
+
+    end
+  end
+end
+TEXT
+
+      File.write( filename, model )
+    end
+  
+end
