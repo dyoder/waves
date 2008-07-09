d0b39731347148eef7dc1b663570d2552b1f508b
Matthew King <automatthew@gmail.com>
2008-07-08 19:24:28 -0500

    specs for app generation now check for root folders

--- verify/app_generation/startup.rb
+++ verify/app_generation/startup.rb
@@ -5,26 +5,35 @@ require File.join(File.dirname(__FILE__) , "helpers")
 clobber_test_app(AppPath)
 generate_test_app(AppPath)
 
-describe "startup.rb in a generated app" do
+describe "A generated application" do
   
-  it "should make the scripts executable" do
+  it "has executable script files" do
     File.executable?(File.join(AppPath, "bin", "waves-console")).should.be.true
   end
 
-  it "should fail to find waves framework, if gem is not available" do
+  it "will fail to find waves framework, if the gem is not available" do
     Dir.chdir(AppPath) do
       stdin, stdout, stderr = Open3.popen3("ruby startup.rb")
       stderr.gets.should =~ %r{no such file to load -- waves \(LoadError\)}
     end
   end
 
-  it "should find and use waves framework source when WAVES is defined" do
+  it "will find and use waves framework source when WAVES is defined" do
     set_framework_path(AppPath)
     Dir.chdir(AppPath) do
       stdin, stdout, stderr = Open3.popen3("ruby startup.rb")
       stderr.gets.should == nil
     end
   end
+  
+  it "has the directories we expect it to have" do
+    dirs = %w{  bin/ configurations/ controllers/ doc/ helpers/ lib/ 
+                log/ models/ public/ schema/ templates/ tmp/ views/ }
+    Dir.chdir(AppPath) do
+      Dir.glob("*/").should == dirs
+    end
+  end
+  
 end
 
 clobber_test_app(AppPath)
