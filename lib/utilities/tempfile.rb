2f309641638de8cbeda33be0ecb15846c36f7109
Matthew King <automatthew@gmail.com>
2008-07-11 16:51:54 -0500

    more rdoc tweaks

--- lib/utilities/tempfile.rb
+++ lib/utilities/tempfile.rb
@@ -1,3 +1,9 @@
+# Patch Ruby's Tempfile class to work around a problem uploading files with Rack.
+# 
+# The fix came from here: http://sinatra.lighthouseapp.com/projects/9779/tickets/1
+# 
+# This is fixed in Rack already, but it hasn't made it into a gem.
 class Tempfile
+  # Make == act like eql?
   def ==(other) ; eql?(other) || super ; end
 end
