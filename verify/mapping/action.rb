03a3c8a2a395daaa1b46bbbe4be5dd99a325ff29
Matthew King <automatthew@gmail.com>
2008-07-21 09:43:34 -0500

    more action mapping specs

--- verify/mapping/action.rb
+++ verify/mapping/action.rb
@@ -1,25 +1,75 @@
 # require 'test_helper' because RubyMate needs help
 require File.join(File.dirname(__FILE__) , "helpers")
 
-describe "A named action mapping"  do
+# N.B.  All of these specs require that the dispatcher actually work.  Unit testing somewhere else
+# will help prevent mysterious spec failures or regressions.
+
+describe "An action mapping"  do
   
   before do
     mapping.clear
     @resource = mock('resource')
-    ResourceMappingApp::Resources::Default.stub!(:new).and_return(@resource)
+    @default = ResourceMappingApp::Resources::Default
+    
+  end
+  
+  it "uses app::Resources::Default when a resource is not specified" do
+    mapping.action( :mapping_name, :get => [ "first", "second"] ) do
+      self.class.inspect
+    end
+    
+    request.get("/first/second").body.should == "ResourceMappingApp::Resources::Default"
+  end
+  
+  it "may specify a resource in the options with a key of :resource" do
+    mapping.action( :mapping_name, :resource => :smurf, :get => [ "first", "second"] ) do
+      self.class.inspect
+    end
+    
+    request.get("/first/second").body.should == "ResourceMappingApp::Resources::Smurf"
+  end
+  
+  it "may determine the resource using a parameter match in the path pattern" do
+    mapping.action( :mapping_name, :get => [ :resources ]) { self.class.inspect }
+    mapping.action( :mapping_name, :get => [ :resource ]) { self.class.inspect }
+    
+    request.get("/blankets").body.should == "ResourceMappingApp::Resources::Blanket"
+    request.get("/blanket").body.should == "ResourceMappingApp::Resources::Blanket"
+  end
+  
+end
+
+describe "A named mapping" do
+  
+  before do
+    mapping.clear
+    @resource = mock('resource')
+    @default = ResourceMappingApp::Resources::Default
   end
   
   it "calls the method with the action name on the Resource" do
+    @default.stub!(:new).and_return(@resource)
     @resource.should.receive(:smurf)
     mapping.action( :smurf, :get => [ "blue_critter" ] )
     
     request.get("/blue_critter").status.should == 200
   end
   
+  it "defines a method on the Resource when a block is given" do
+    mapping.action( :gargamel, :get => [ "wizard" ] ) { "Kill Smurfs!" }
+    
+    @default.new( mock('request') ).should.respond_to :gargamel
+  end
+  
+  it "defines a path generator on the Resource's Paths object" do
+    mapping.action( :gargamel, :get => [ "wizard" ] ) { "Kill Smurfs!" }
+    
+    @default.new( mock('request') ).paths.should.respond_to :gargamel
+  end
   
 end
 
-describe "An anonymous action mapping" do
+describe "An anonymous mapping" do
   
   before do
     mapping.clear
