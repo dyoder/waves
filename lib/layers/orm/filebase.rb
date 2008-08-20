5793b0af4e536c190fd4539b702efe8bd3b1c4dc
Matthew King <automatthew@gmail.com>
2008-08-20 16:32:26 -0500

    filebase ORM uses only model basename for path

--- lib/layers/orm/filebase.rb
+++ lib/layers/orm/filebase.rb
@@ -2,13 +2,16 @@ module Waves
   module Layers
     module ORM
       
-      # Work in Progress
+      # The Filebase ORM layer configures model classes to use Filebase with a datastore located in
+      # <tt>db/model_name</tt>, where @model_name@ is the snakecased version of the class name.
+      #
+      # The Filebase store for Blog::Models::Entry would be located in <tt>db/entry</tt>, for example.
       module Filebase
         
         def self.included(app)
           app.module_eval do
             auto_eval( :Models ) do
-              auto_eval( true ) { include ::Filebase::Model[ :db / self.name.snake_case ] }
+              auto_eval( true ) { include ::Filebase::Model[ :db / self.basename.snake_case ] }
             end
           end
         end
