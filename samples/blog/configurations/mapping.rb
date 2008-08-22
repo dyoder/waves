5b5d45c242de1bc9ac63498f5964407ed6c9a330
Matthew King <automatthew@gmail.com>
2008-08-22 00:01:17 -0500

    updated samples/blog mappings to the New Way

--- samples/blog/configurations/mapping.rb
+++ samples/blog/configurations/mapping.rb
@@ -6,20 +6,20 @@ module Blog
       
       extend Waves::Mapping
       
-      response( :get => [] ) { redirect "/waves/status"}
-      response( :get => [ "waves", "status" ]) { render "waves/status" }
+      on( :get => [] ) { redirect "/waves/status"}
+      on( :get => [ "waves", "status" ]) { render "waves/status" }
       
       # specific to comments - on create redirect to the entry, not the comment itself
-      response :create, :resource => :comment, :post => [ 'comments' ] do
-        redirect( Blog.paths( :entry ).read( controller( :create ).entry.name ) )
+      on :post => [ 'comments' ], :resource => :comment, :as => :create do
+        redirect( paths( :entry ).read( controller( :create ).entry.name ) )
       end
       
       # defaults for generic resources
-      response( :list, :get => [ :resources ] ) { controller( :all ) and view( :list ) }
-      response( :create, :post => [ :resources ] ) { redirect( paths.read( controller( :create ).name, 'edit' ) ) }
-      response( :read, :get => [ :resource, :name, { :mode => 'show' } ] ) { controller( :find, name ) and view( mode ) }
-      response( :update, :put => [ :resource, :name ] ) { controller( :update, name ) and redirect( paths.read( name ) ) }
-      response( :delete, :delete => [ :resource, :name ] ) { controller( :delete, name ) }
+      on( :get => [ :resources ], :as => :list ) { controller( :all ) and view( :list ) }
+      on( :post => [ :resources ], :as => :create ) { redirect( paths.read( controller( :create ).name, 'edit' ) ) }
+      on( :get => [ :resource, :name, { :mode => 'show' } ], :as => :read ) { controller( :find, name ) and view( mode ) }
+      on( :put => [ :resource, :name ], :as => :update ) { controller( :update, name ) and redirect( paths.read( name ) ) }
+      on( :delete => [ :resource, :name ], :as => :delete ) { controller( :delete, name ) }
       
     end  
 
