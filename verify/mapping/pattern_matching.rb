939cfa2aa52743884698d568302a99499979ac0e
Matthew King <automatthew@gmail.com>
2008-08-21 21:54:24 -0500

    pattern matching specs updated for The New Way

--- verify/mapping/pattern_matching.rb
+++ verify/mapping/pattern_matching.rb
@@ -6,15 +6,18 @@ describe "In a mapping's path-matcher"  do
   describe "the HTTP method" do
     
     before do
-      mapping.clear
+      mappings.clear
       handle( Waves::Dispatchers::NotFoundError ) { response.status = 404 }
     end
   
     it "is specified with a hash key" do
-      mapping.response( :get => [ 'somewhere' ] )     { "GET method" }
-      mapping.response( :put => [ 'somewhere' ] )     { "PUT method" }
-      mapping.response( :post => [ 'somewhere' ] )    { "POST method" }
-      mapping.response( :delete => [ 'somewhere' ] )  { "DELETE method" }
+      mappings do
+        on( :get => [ 'somewhere' ] )     { "GET method" }
+        on( :put => [ 'somewhere' ] )     { "PUT method" }
+        on( :post => [ 'somewhere' ] )    { "POST method" }
+        on( :delete => [ 'somewhere' ] )  { "DELETE method" }
+      end
+
     
       mock_request.get("/somewhere").body.should == "GET method"
       mock_request.put("/somewhere").body.should == "PUT method"
@@ -25,72 +28,102 @@ describe "In a mapping's path-matcher"  do
   end
   
   describe "the request path pattern" do
+      
+      before do
+        mappings.clear
+        handle( Waves::Dispatchers::NotFoundError ) { response.status = 404 }
+      end
     
-    before do
-      mapping.clear
-      handle( Waves::Dispatchers::NotFoundError ) { response.status = 404 }
+      it "is an array given as the value of the HTTP method key" do
+        mappings do
+          on( :get => [ 'foo' ] )  { 'The Foo' }
+          on( :get => [ 'bar', 'baz' ] )  { 'The Bar' }
+        end
+        
+        mock_request.get('/foo').body.should == 'The Foo'
+        mock_request.get('/bar/baz').body.should == 'The Bar'
+      end
+      
+      it "may be omitted to allow matching all paths" do
+        mappings do
+          on( {} )  { "pathless" }
+        end
+        
+        mock_request.get('/bogus').body.should == 'pathless'
+      end
+      
+      it "may use true to match all paths, while specifying a method" do
+        mappings do
+          on( :get => [ true ] ) { "pathless" }
+        end
+        
+        mock_request.get('/bogus').body.should == 'pathless'
+      end
+      
+      it "may use true as the last element, to match all remaining components" do
+        mappings do
+          on( :get => [ "first", true ] ) { "Yes" }
+        end
+        
+        mock_request.get('/first/second/third').body.should == "Yes"
+        mock_request.get('/uno/dos/tres').status.should == 404
+      end
+      
+      it "may match string literals" do
+        mappings do
+          on( :get => [ 'kilroy', 'was', 'here'] )  { 'Hello from Kilroy' }
+        end
+        
+        mock_request.get('/kilroy/was/here').body.should == 'Hello from Kilroy'
+        
+        # doesn't work for partial matches.
+        mock_request.get('/kilroy/was').status.should == 404
+      end
+      
+      it "may use symbols as placeholders for a default regex" do
+        mappings do
+          on( :get => [ :critter, :name ] ) { "hi" }
+        end
+        
+        mock_request.get('/smurf/papa_smurf').status.should == 200
+        
+      end
+      
+      # it "may use hashes to specify placeholders with custom regexes" do
+      #   mapping.on( :get => [ :prisoner, { :prisoner_id => /9430|24601/ } ] ) { "I am Jean Valjean!" }
+      #   
+      #   mock_request.get("/prisoner/9430").body.should == "I am Jean Valjean!"
+      #   mock_request.get("/prisoner/9431").status.should == 404
+      # end
+      
+      it "saves placeholder matches as params" do
+        mappings do
+          on( :get => [ :critter, :name ]) { "#{params['critter']}, #{params['name']}" }
+        end
+        
+        mock_request.get('/dwarf/tyrion').body.should == "dwarf, tyrion"
+      end
+      
+      it "may use regexes instead of strings or placeholders, but the matches are not saved" do
+        # extremely bad example.  Don't ever really use this for authentication.  If somebody thinks up a
+        # better use case for pattern components that throw away the matches, please fix this spec.
+        mappings do
+          on( :get => [ "user", /matthew|mark/, "password", /wehttam|kram/ ]) { "you been let in"}
+        end
+        
+        mock_request.get('/user/matthew/password/wehttam').status.should == 200
+        mock_request.get('/user/matthew/password/nimda').status.should == 404
+      end
+      
+      it "can apparently also use hashes to set default values" do
+        mappings do
+          on( :get => [ "thingy", { :mode => 'show' } ] ) { "mode: #{params['mode']}" }
+        end
+        
+        mock_request.get("/thingy").body.should == "mode: show"
+        mock_request.get("/thingy/edit").body.should == "mode: edit"
+      end
+      
     end
-  
-    it "is an array given as the value of the HTTP method key" do
-      mapping.response( :get => [ 'foo' ] )  { 'The Foo' }
-      mapping.response( :get => [ 'bar', 'baz' ] )  { 'The Bar' }
-      
-      mock_request.get('/foo').body.should == 'The Foo'
-      mock_request.get('/bar/baz').body.should == 'The Bar'
-    end
-    
-    it "may be omitted to allow matching all paths" do
-      mapping.response( {} )  { "pathless" }
-      
-      mock_request.get('/bogus').body.should == 'pathless'
-    end
-    
-    it "may match string literals" do
-      mapping.response( :get => [ 'kilroy', 'was', 'here'] )  { 'Hello from Kilroy' }
-      
-      mock_request.get('/kilroy/was/here').body.should == 'Hello from Kilroy'
-      
-      # doesn't work for partial matches.
-      mock_request.get('/kilroy/was').status.should == 404
-    end
-    
-    it "may use symbols as placeholders for a default regex" do
-      mapping.response( :get => [ :critter, :name ]) { "hi" }
-      
-      mock_request.get('/smurf/papa_smurf').status.should == 200
-      
-      # The default regex accepts word characters, hyphens, and underscores only.
-      mock_request.get('/smurf/moo+cow').status.should == 404
-    end
-    
-    # it "may use hashes to specify placeholders with custom regexes" do
-    #   mapping.response( :get => [ :prisoner, { :prisoner_id => /9430|24601/ } ] ) { "I am Jean Valjean!" }
-    #   
-    #   mock_request.get("/prisoner/9430").body.should == "I am Jean Valjean!"
-    #   mock_request.get("/prisoner/9431").status.should == 404
-    # end
-    
-    it "saves placeholder matches as params" do
-      mapping.response( :get => [ :critter, :name ]) { "#{params['critter']}, #{params['name']}" }
-      
-      mock_request.get('/dwarf/tyrion').body.should == "dwarf, tyrion"
-    end
-    
-    it "may use regexes instead of strings or placeholders, but the matches are not saved" do
-      # extremely bad example.  Don't ever really use this for authentication.  If somebody thinks up a
-      # better use case for pattern components that throw away the matches, please fix this spec.
-      mapping.response( :get => [ "user", /matthew|mark/, "password", /wehttam|kram/ ]) { "you were able to get logged in!!!!"}
-      
-      mock_request.get('/user/matthew/password/wehttam').status.should == 200
-      mock_request.get('/user/matthew/password/nimda').status.should == 404
-    end
-    
-    it "can apparently also use hashes to set default values" do
-      mapping.response( :get => [ "view", { :mode => 'show' } ] ) { "mode: #{params['mode']}" }
-      
-      mock_request.get("/view").body.should == "mode: show"
-    end
-    
-  end
   
 end
