d069bd05b953137e4c05c2ebe8448419be0d2d66
Matthew King <automatthew@gmail.com>
2008-09-03 11:12:53 -0500

    removing jblog for lack of usefulness

--- samples/jblog/startup.rb
+++
@@ -1,6 +0,0 @@
-WAVES = File.join(File.dirname(__FILE__), "..", "..")
-lambda {
-  waves = ( ( WAVES if defined? WAVES ) || ENV['WAVES'] || File.join(File.dirname(__FILE__), 'waves') )
-  $:.unshift(File.join( waves, "lib" )) if File.exist? waves
-}.call
-require 'waves'
