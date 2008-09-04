22c0672bdf015a9fea0f5f3cb015aa055c007f98
ab5tract <ethyrnet@yahoo.com>
2008-09-04 17:16:47 -0400

    FileCache layer is on its way

--- lib/layers/cache/file_cache.rb
+++ lib/layers/cache/file_cache.rb
@@ -4,6 +4,42 @@ module Waves
 
     class FileCache < Waves::Cache
 
+      @files = Hash.new
+
+      def store(key, value, ttl = {})
+        item = {
+          :expires => ttl.i_a?(Number) ? Time.now + ttl : nil,
+          :value => value
+        }
+
+        @files[key] = File.new(key,'w') if @files[key].nil?
+        Marshal.dump(item, @file[key])
+      end
+
+      def delete(*keys)
+        keys.each do |key|
+          File.delete(@files[key])
+          @files[key].delete
+        end
+      end
+
+      def clear
+        # in a sec...
+      end
+
+      def fetch(key)
+        raise "No cache found for #{key}" unless @files[key]
+        item = Marshal.load(@files[key])
+
+        if item[:expires] > Time.now
+          item[:value]
+        else
+          File.delete(@files[key])
+          @files[key].delete
+          item[:value]
+        end
+      end
+
     end
 
   end
