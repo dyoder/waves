d15842d37317b3fa78024010894b7ffeac3124d8
Matthew King <automatthew@gmail.com>
2008-09-21 15:44:21 -0500

    undo switch from pop to shift, 'cause something's fishy

--- samples/blog/resources/entry.rb
+++ samples/blog/resources/entry.rb
@@ -1,45 +1,35 @@
 module Blog
   module Resources
     class Entry < Default
-
-      raise "got to resource"
-
-      on :get, :list => [ ] do
-        raise  "list"
+      
+      
+      on :get, :list => [ 'entry' ] do
+        view.list( plural => controller.all )
       end
       
-      on :get, :show => [ :name ] do
-        raise "show: #{captured.name}"
+      on :get, :show => [ 'entry', :name ] do
+        view.show( singular => controller.find( query.name ) )
       end
       
-      # 
-      # on :get, :list => [ ] do
-      #   view.list( plural => controller.all )
-      # end
-      # 
-      # on :get, :show => [ :name ] do
-      #   view.show( singular => controller.find( query.name ) )
-      # end
-      # 
-      # on :get, :edit => [ :name, 'edit' ] do
-      #   view.edit( singular => controller.find( query.name ) )
-      # end
-      # 
-      # on :put, :update => [ :name ] do
-      #   controller.update( query.name )
-      #   redirect  "/entry/#{query.name}"
-      # end
-      # 
-      # on :post, :create => [ ] do
-      #   redirect "/entry/#{controller.create.name}/edit"
-      # end
-      # 
-      # on :post, :comment => [ :name ] do
-      #   entry = controller.find( query.name )
-      #   comment = Models::Comment.create( query.comment.to_hash )
-      #   entry.add_comment( comment )
-      #   redirect request.path
-      # end
+      on :get, :edit => [ 'entry', :name, 'edit' ] do
+        view.edit( singular => controller.find( query.name ) )
+      end
+      
+      on :put, :update => [ 'entry', :name ] do
+        controller.update( query.name )
+        redirect  "/entry/#{query.name}"
+      end
+      
+      on :post, :create => [ 'entry' ] do
+        redirect "/entry/#{controller.create.name}/edit"
+      end
+      
+      on :post, :comment => [ 'entry', :name ] do
+        entry = controller.find( query.name )
+        comment = Models::Comment.create( query.comment.to_hash )
+        entry.add_comment( comment )
+        redirect request.path
+      end
       
     end
   end
