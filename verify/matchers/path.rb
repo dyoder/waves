fa964ade5c5db00bd79064b967f25ead35d77866
Matthew King <automatthew@gmail.com>
2008-09-21 15:07:49 -0500

    reorged the path matching specs

--- verify/matchers/path.rb
+++ verify/matchers/path.rb
@@ -1,26 +1,34 @@
 # require 'test_helper' because RubyMate needs help
 require File.join(File.dirname(__FILE__) , "helpers")
 
+Waves::Matchers::Path.module_eval do
+  def matches?(request)
+    self.call(request).kind_of?(Hash)
+  end
+end
+
 describe "A path matcher with a single string component" do
   
   before do
     @matcher = Waves::Matchers::Path.new( [ "smurf" ] )
   end
   
-  it "matches a single-component url with the string" do
-    @matcher.call(waves_request("/smurf")).should == {}  
+  it "matches a single-component path consisting of the string" do
+    @matcher.matches?(waves_request("/smurf")).should == true 
   end
   
-  it "does not match a single-component url with a different string" do
-    @matcher.call( waves_request("/bad") ).should == nil
+  it "does not match a single-component path with a different string" do
+    @matcher.matches?( waves_request("/bad") ).should == false
   end
   
-  it "does not match a multi-component url with the string as the first component" do
-    @matcher.call( waves_request("/smurf/close") ).should == nil
+  it "does not match a multi-component path" do
+    @matcher.matches?( waves_request("/smurf/close") ).should == false
+    @matcher.matches?( waves_request("/not/even/close") ).should == false
+    @matcher.matches?( waves_request("/not/smurf/close") ).should == false
   end
-  
-  it "does not match a multi-component url entirely without the string" do
-    @matcher.call( waves_request("/not/even/close") ).should == nil
+    
+  it "does not match '/' " do
+    @matcher.matches?( waves_request("/")).should == false
   end
   
 end
@@ -31,17 +39,32 @@ describe "A path matcher with multiple string components" do
     @matcher = Waves::Matchers::Path.new( [ "smurf", "pie" ] )
   end
   
-  it "matches when all url components match" do
-    @matcher.call(waves_request("/smurf/pie")).should == {}
+  it "matches when all path components match" do
+    @matcher.matches?(waves_request("/smurf/pie")).should == true
   end
   
-  it "does not match when no url components match" do
-    @matcher.call(waves_request("/foo/bar")).should == nil
+  it "does not match unless all path components match" do
+    @matcher.matches?(waves_request("/foo/bar")).should == false
+    @matcher.matches?(waves_request("/smurf")).should == false
+    @matcher.matches?(waves_request("/smurf/cake")).should == false
+    @matcher.matches?(waves_request("/smurf/pie/filling")).should == false
+    @matcher.matches?(waves_request("/filling/smurf/pie")).should == false
+  end
+
+end
+
+describe "A path matcher with a single capture component" do
+  
+  before do
+    @matcher = Waves::Matchers::Path.new( [ :resource ] )
   end
   
-  it "does not match when only the first component matches" do
-    @matcher.call(waves_request("/smurf/cake")).should == nil
-    @matcher.call(waves_request("/smurf/pie/filling")).should == nil
+  it "matches any single-component path" do
+    @matcher.matches?(waves_request("/smurf")).should == true
   end
   
 end
+
+
+
+
