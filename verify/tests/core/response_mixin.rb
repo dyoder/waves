af46bbf57873977c1ba381969e719c70cdd63868
Matthew King <automatthew@gmail.com>
2008-10-03 11:05:25 -0500

    partially complete ResponseMixin tests, but with tons of mocking/stubbing

--- verify/tests/core/response_mixin.rb
+++ verify/tests/core/response_mixin.rb
@@ -1,12 +1,11 @@
 # require 'test_helper' because RubyMate needs help
 require File.join(File.dirname(__FILE__) , "helpers")
 
-# also moved model, view, controller methods for ResponseMixin to mvc layer 
-# 
-# all now rely on Functor and that establishes the multi-app pattern. technically the one argument versions should access the app they are defined within, but that can be incorporated later.
-# http://gist.github.com/1464
-
 module Thingy
+  module Resources
+    class Hoedown; end
+    class Default; end
+  end
   module Controllers
     class Default
 
@@ -24,39 +23,45 @@ end
 describe "The ResponseMixin module" do
   
   before do
-    fake_out_runtime
     @thingy = Thingy::Controllers::Default.new
   end
-  
-  # it "defines a helper for accessing named resources" do
-  #   @thingy.resource(:mookie).should == VerifyCore::Resources::Mookie
-  #   @thingy.resource(:verify_core, :mookie).should == VerifyCore::Resources::Mookie
-  # end
-  
+    
   it "defines helpers for reaching the application" do
     @thingy.app.should == Thingy
     @thingy.app_name.should == :thingy
   end
 
-# check to see which methods are actually delegated  
   it "defines delegators for request methods" do
-    methods = [ :response, :session, :path, :url, :domain, :blackboard, :not_found ]
-    methods.each do |method|
-      @thingy.request.should.receive(method)
-    end
+    methods = [ :response, :session, :path, :url, :domain, :not_found, :traits ]
+    methods.each { |method| @thingy.request.should.receive(method) }
     
-    methods.each do |method|
-      @thingy.send(method)
-    end
+    methods.each { |method| @thingy.send(method) }
+  end
+  
+  it "delegates delegators to traits.waves" do
+    methods = [ :captured, :resource ]
+    methods.each { |method| @thingy.traits.waves.should.receive( method ) }
+    
+    methods.each { |method| @thingy.send(method) }
   end
   
   it "destructures params now" do
     # should.flunk "write a test, fool."
   end
   
-  it "defines a delegator for the application's paths" do
-    Thingy.should.receive(:paths).with(:meek)
-    @thingy.paths(:meek)
+  it "defines a method for accessing the present resource paths" do
+    paths = mock('paths')
+    paths.should.receive(:new).with(@thingy.request)
+    Thingy::Resources::Default.should.receive(:paths).and_return(paths)
+    @thingy.should.receive(:resource).and_return(Thingy::Resources::Default.new)
+    @thingy.paths
+  end
+  
+  it "defines a method for accessing other resources' paths" do
+    paths = mock('paths')
+    paths.should.receive(:new).with(@thingy.request)
+    Thingy::Resources::Hoedown.should.receive(:paths).and_return(paths)
+    @thingy.paths(:Hoedown)
   end
   
   it "defines a redirect method with default status code" do
