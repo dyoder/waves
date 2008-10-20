168eb78da2b062ff42ca6618bd226ad8d3dd0563
Matthew King <automatthew@gmail.com>
2008-10-20 12:21:28 -0500

    squeeze cache specs into proper form

--- verify/specs/caches/simple.rb
+++ verify/specs/caches/simple.rb
@@ -4,23 +4,21 @@ require 'caches/simple'
 
 module SimpleApp ; include Waves::Foundations::Compact ; end
 
-describe "can assign a Waves::Caches::Simple object to Waves.cache" do
-  module SimpleApp
+describe "Waves.cache" do
+  it "Takes a cache object as its argument" do
     Waves.cache Waves::Caches::Simple.new
   end
 end
 
-describe "can store and fetch, including implementation-wide methods" do
-  module SimpleApp
+describe "Waves::Caches::Simple" do
+  it "can store and fetch, including implementation-wide methods" do
     Waves.cache.store :frog, "hopping"
     Waves.cache[:ball] = "dropping"
         
     Waves.cache[:frog].should.==("hopping") and Waves.cache.fetch(:ball).should.==("dropping")
   end
-end
 
-describe "can delete and clear" do
-  module SimpleApp
+  it "can delete and clear" do
     Waves.cache.delete :frog
     Waves.cache.fetch(:frog).should == nil
     
