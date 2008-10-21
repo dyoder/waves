42ab106ba08ceb8de67bda15449ac94b6ff2085b
Matthew King <automatthew@gmail.com>
2008-10-21 10:28:18 -0500

    updated sample blog to match framework

--- samples/blog/configurations/development.rb
+++ samples/blog/configurations/development.rb
@@ -7,24 +7,17 @@ module Blog
       database :adapter => 'sqlite', :database => 'blog.db'
 
       reloadable [ Blog ]
-
       log :level => :debug
-      
-      session :duration => 45.minutes, :path => 'tmp/sessions'
-
       host '127.0.0.1'
-
       port 4000
-
-      handler ::Rack::Handler::Mongrel, :Host => host, :Port => port
-      # handler ::Rack::Handler::WEBrick, :BindAddress => host, :Port => port
-      # handler ::Rack::Handler::Thin, :Host => host, :Port => port
 
       application do
         use ::Rack::ShowExceptions
         use ::Rack::Static, :urls => [ '/images/', '/css/', '/javascript/', '/favicon.ico' ], :root => 'public'
         run ::Waves::Dispatchers::Default.new
       end
+      
+      server Waves::Servers::Mongrel
 
     end
 
