055269177d63ca9d8facab06cac73ade7f62b369
Matthew King <automatthew@gmail.com>
2008-10-21 09:36:48 -0500

    remove old app template

--- app/startup.rb
+++
@@ -1,5 +0,0 @@
-lambda {
-  waves = ( ( WAVES if defined? WAVES ) || ENV['WAVES'] || File.join(File.dirname(__FILE__), 'waves') )
-  $:.unshift(File.join( waves, "lib" )) if File.exist? waves
-}.call
-require 'waves'
