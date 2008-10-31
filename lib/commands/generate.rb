478313355e390c0a1b01e1eb18389bc826d1dd64
Dan Yoder <dan@zeraweb.com>
2008-10-30 20:02:01 -0700

    Refactored app generation.

--- lib/commands/generate.rb
+++ lib/commands/generate.rb
@@ -1,88 +1,50 @@
 require 'choice'
 require 'rakegen'
 
+waves = File.expand_path( File.dirname( __FILE__ ) / '..' / '..' )
+orms = Dir[ waves / :lib / :layers / :orm / :providers / '*.rb' ].map { |path| File.basename( path, '.rb' )}
+templates = Dir[ waves / :templates / '*' ].map { |path| File.basename( path ) }
+
 Choice.options do
-  banner 'Usage:  waves path/to/app [-h]'
+  
   option :help  do
     long '--help'
     desc 'Show this message'
   end
   
-#  option :generate do
-    option :orm do
-      short '-o'
-      long '--orm=ORM'
-      desc "Select an ORM (e.g. active_record, sequel, none)"
-      default "sequel"
-    end
-  
-    option :template do
-      short '-t'
-      long '--template=FOUNDATION'
-      desc "Select a template for app generation (Built-in options: 'classic', 'compact')."
-      default "classic"
-    end
-#  end
-  
-end
-
-available_orms = [ 'sequel' ,  'active_record' , 'none' ]
-orm = Choice.choices.orm.snake_case
-orm_require, orm_include = case orm
-when 'sequel'
-  ["require 'layers/orm/sequel'", "include Waves::Layers::ORM::Sequel"]
-when /active([-_]*)record/
-  ["require 'layers/orm/active_record'", "include Waves::Layers::ORM::ActiveRecord"]
-when 'none'
-  ['', '# This app was generated without an ORM layer.']
-else
-  puts "I'm sorry, '#{orm}' is not listed as an available option.\nTry: \t"
-  available_orms.each { |x| puts "\t\t#{x}" }
-  raise ArgumentError
-end
-
-app_path = ARGV[1]
-app_name = File.basename(app_path)
-if app_name =~ /[^\w\d_]/
-  raise ArgumentError, <<-TEXT
-  Unusable name: \"#{app_name}\"
-  Application names may contain only letters, numbers, and underscores."
-TEXT
-end
-
-skip_rake = false
-
-case Choice.choices.template
-when 'classic'
-  template = File.exist?(Choice.choices.template) ? Choice.choices.template : "#{WAVES}/templates/classic"
-when 'compact'
-  compact_app = <<-COMPACT
-require 'foundations/compact'
-
-module #{app_name}
-  include Waves::Foundations::Compact
-
-  module Resources
-    class Map
-    end
-
+  option :orm do
+    short '-o'
+    long '--orm=ORM'
+    desc "Select an ORM (currently supported: #{orms * ', '} )"
+    valid orms
   end
-end
-COMPACT
-  skip_rake = true
-  File.open(app_path + '.rb', 'w') {|file| file.print compact_app}
-else
-  template = Choice.choices.template
+
+  option :template do
+    short '-t'
+    long '--template=classic'
+    desc "Select a template for your app (options: #{templates * ', '})."
+    valid templates
+  end
+
+  option :name, :required => true do
+    short '-n'
+    long '--name'
+    desc "Select a name for the application. Use only letters, numbers, dashes, or underscores."
+    validate /^[\w\d\-]+$/
+  end
+
 end
 
-generator = Rakegen.new("waves:app") do |gen|
-  gen.source = template
-  gen.target = app_path
-  gen.template_assigns = {:name => app_name.camel_case, :orm_require => orm_require, :orm_include => orm_include }
-end unless skip_rake
+options = Choice.choices
 
 puts "** Creating new Waves application ..."
 
-Rake::Task["waves:app"].invoke unless skip_rake
+# why do i have to do this?
+FileUtils.mkdir( File.expand_path( options.name ) )
+generator = Rakegen.new("generate") do |gen|
+  gen.source = waves / :templates / options.template
+  gen.target = File.expand_path( options.name )
+  gen.template_assigns = options.merge( :name => options.name.gsub('-','_').camel_case )
+end.invoke
 
 puts "** Application created!"
