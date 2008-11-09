a21be406cc7ea0b98e52d02ed54210cf55e8ca78
Dan Yoder <dan@zeraweb.com>
2008-11-09 03:19:54 -0800

    ERb was no longer working due to #render method being defined in ResponseMixin. Also, seemed like it might have been more complicated than necessary.

--- lib/layers/renderers/erubis.rb
+++ lib/layers/renderers/erubis.rb
@@ -1,34 +1,5 @@
 require 'erubis'
 
-module Erubis # :nodoc:
-
-  # This is added to the Erubis Content class to allow the same helper methods
-  # to be used with both Markaby and Erubis.
-  class Context
-    
-    def <<(s) 
-      eval("_buf << '#{s}'", @binding)
-    end
-    
-    def capture
-      eval("_context.push(_buf); _buf = ''", @binding) #ignore output from that eval, will be added via "<<"
-      result = Erubis::Eruby.new(yield).result @binding
-      eval("_buf = _context.pop", @binding)
-      result
-    end
-    
-    def to_s(eruby)
-      unless @binding
-        @binding = binding
-        eval("_buf = ''; _context = []", @binding)
-      end
-      eruby.result @binding
-    end
-
-  end
-
-end
-
 module Waves
   
   module Renderers
@@ -42,14 +13,12 @@ module Waves
       def self.render( path, assigns )
         eruby = ::Erubis::Eruby.new( template( path ) )
         helper = helper( path )
-        context = ::Erubis::Context.new
-        context.meta_eval { include( helper ) ; }
-        context.instance_eval do
-          assigns.each do |key,val|
-            instance_variable_set("@#{key}",val)
-          end
+        context = ::Erubis::Context.new( assigns )
+        ( class << context ; self ; end ).module_eval do 
+          include( helper )
+          def << (s) ; s ; end
         end
-        context.to_s(eruby)
+        eruby.evaluate( context )
       end
 
 
