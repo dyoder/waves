bddc5732dc55b44e1d9661efeabd5867e29f2de5
Matthew King <automatthew@gmail.com>
2008-12-29 11:29:01 -0600

    path generation fixes for wants of regex and hash

--- lib/resources/paths.rb
+++ lib/resources/paths.rb
@@ -11,21 +11,33 @@ module Waves
       def initialize( request ) ; @request = request ; end
       
       def generate( template, args )
-        return "/#{ args * '/' }" unless template.is_a?( Array ) and not template.empty?
-        path = []
-        ( "/#{ path * '/' }" ) if template.all? do | want |
-          case want
-          when true then path += args
-          when String then path << want
-          when Symbol then path << args.shift
-          when Regexp then path << args.shift
-          when Hash
-            key, value = want.to_a.first
-            case value
+        if template.is_a? Array and not template.empty?
+          path = []
+          ( "/#{ path * '/' }" ) if template.all? do | want |
+            case want
             when true then path += args
-            when String, Symbol, RegExp then path << args.unshift
+            when String then path << want
+            when Symbol then path << args.shift
+            when Regexp
+              component = args.shift.to_s
+              raise ArgumentError, "#{component} does not match #{want.inspect}" unless component =~ want
+              path << component
+            when Hash
+              key, value = want.to_a.first
+              case value
+              when true then path += args
+              when String, Symbol
+                # if no args to interpolate, use hash element value as default
+                !args.empty? ? path << args.shift : path << value
+              when Regexp
+                component = args.shift.to_s
+                raise ArgumentError, "#{component} does not match #{want.inspect}" unless component =~ value
+                path << component
+              end
             end
           end
+        else
+          "/#{ args * '/' }"
         end
       end
     end
