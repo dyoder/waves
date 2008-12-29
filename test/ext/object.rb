9641079395c19f414f358b6ad4b3138a002e4bf2
Matthew King <automatthew@gmail.com>
2008-12-29 14:54:36 -0600

    tests for Object#cache_method_missing

---
+++ test/ext/object.rb
@@ -0,0 +1,43 @@
+require "#{File.dirname(__FILE__)}/../../test/helpers.rb"
+
+describe "Object#cache_method_missing" do
+  
+  before do
+    class A; end
+  end
+  
+  after do
+    Object.instance_eval { remove_const(:A) if const_defined?(:A) }
+  end
+  
+  it "defines the missing method" do
+    A.module_eval do
+      def method_missing(name, *args)
+        cache_method_missing name, "'hi'", *args
+      end
+    end
+    A.new.bar
+    A.new.should.respond_to :bar
+  end
+  
+  it "passes along the args" do
+    A.module_eval do
+      def method_missing(name, *args, &block)
+        cache_method_missing name, "args.join('-')", *args
+      end
+    end
+    
+    A.new.bar(1, 2, 3).should == "1-2-3"
+  end
+  
+  it "passes along the block" do
+    A.module_eval do
+      def method_missing(name, *args, &block)
+        cache_method_missing name, "block.call", *args, &block
+      end
+    end
+    
+    A.new.bar { 'bye' }.should == 'bye'
+  end  
+  
+end
