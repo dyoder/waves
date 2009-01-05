cc1b31125a369cc1e2a037e5014aa63ea5e620f0
Matthew King <automatthew@gmail.com>
2009-01-05 17:11:54 -0600

    refactoring Path matcher for potential speed and clarity increases

--- lib/matchers/path.rb
+++ lib/matchers/path.rb
@@ -7,58 +7,54 @@ module Waves
       # Takes an array of pattern elements ... coming soon, support for formatted strings!
       def initialize( pattern ) ; @pattern = pattern  ; end
       
+      # returns a hash of captured values
       def call( request )
-        return {} if @pattern == true or @pattern == false or @pattern.nil?
-        path = extract_path( request ).reverse
-        return {} if ( path.empty? and @pattern.empty? )
-        capture = {}
-        match =  @pattern.all? do | want |
-          case want
-          when true # means 1..-1
-            path = [] unless path.empty?
-          when Range 
-            if want.end == -1
-              path = [] if path.length >= want.begin
-            else
-              path = [] if want.include? path.length
-            end
-          when String then want == path.pop
-          when Symbol then capture[ want ] = path.pop
-          when Regexp then want === path.pop
-          when Hash
-            key, value = want.to_a.first
-            case value
-            when true
-              ( capture[ key ], path = path.reverse, [] ) unless path.empty?
-            when Range
-              if value.end == -1
-                ( capture[ key ], path = path.reverse, [] ) if path.length >= value.begin
+        if @pattern.is_a? Array
+          path = extract_path( request ).reverse
+          return {} if @pattern.empty? && path.empty?
+          capture = {}
+          match = @pattern.all? do | want |
+            case want
+            when true # same as a Range of 1..-1
+              path = [] unless path.empty?
+            when Range 
+              if want.end == -1
+                path = [] if path.length >= want.begin
               else
-                ( capture[ key ], path = path.reverse, [] ) if value.include? path.length
+                path = [] if want.include? path.length
               end
-            when String, Symbol
-              got = path.pop
-              capture[ key ] = got ? got : value.to_s
-            when Regexp then
-              got = path.pop
-              capture[ key ] = got if value === got
+            when String then want == path.pop
+            when Symbol then capture[ want ] = path.pop
+            when Regexp then want === path.pop
+            when Hash
+              key, value = want.to_a.first
+              case value
+              when true
+                ( capture[ key ], path = path.reverse, [] ) unless path.empty?
+              when Range
+                if value.end == -1
+                  ( capture[ key ], path = path.reverse, [] ) if path.length >= value.begin
+                else
+                  ( capture[ key ], path = path.reverse, [] ) if value.include? path.length
+                end
+              when String, Symbol
+                got = path.pop
+                capture[ key ] = got ? got : value.to_s
+              when Regexp then
+                got = path.pop
+                capture[ key ] = got if value === got
+              end
             end
           end
+          capture if match && path.empty?
+        elsif @pattern == true or @pattern == false or @pattern == nil
+          {}
         end
-        capture if match && path.empty?
       end
 
       # private 
 
       # just a little helper method
-      def original_extract_path( request )
-        path = request.traits.waves.path
-        return path if path
-        path = request.path.split('/').map { |e| Rack::Utils.unescape(e) }
-        path.shift unless path.empty?
-        request.traits.waves.path = path
-      end
-      
       def extract_path( request )
         request.traits.waves.path ||= request.path.scan(/[^\/]+/).map { |e| Rack::Utils.unescape(e) }
       end
