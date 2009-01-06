ee99de5ddbc93e56720bb12149cbca01bd8f85f8
Matthew King <automatthew@gmail.com>
2009-01-06 15:19:06 -0600

    separated specs for params, query, captured

--- test/match/params.rb
+++ test/match/params.rb
@@ -1,23 +1,33 @@
 require "#{File.dirname(__FILE__)}/../../test/helpers.rb"
 require 'foundations/compact'
 
-describe "Query, Captured, and Params" do
+describe "A resource that has matched a request" do
     
   before do
     Test = Module.new { include Waves::Foundations::Compact }
     Waves << Test
+    
+    @request = Waves::Request.new( env('http://localhost/baz?bar=7', :method => 'GET' ) )
+    Test::Resources::Map.on( :get, [ :foo ] ) {}
+    @resource = Test::Resources::Map.new( @request )
+    @resource.get
   end
   
   after do
     Waves.applications.clear
     Object.instance_eval { remove_const(:Test) if const_defined?(:Test) }
   end
-    
-  feature "Params should combined captured and query" do 
-    Test::Resources::Map.on( :get, [ :foo ] ) {}
-    r = Test::Resources::Map.new( Waves::Request.new( env('http://localhost/baz?bar=7', :method => 'GET' ) ) )
-    r.get ; r.captured.to_h.should == { 'foo' => 'baz' }; r.query.to_h.should == { 'bar' => '7' }
-    r.params.to_h.should == { 'foo' => 'baz', 'bar' => '7' }
+  
+  it "makes parameters captured from the path available as #captured" do
+    @resource.captured.to_h.should == { 'foo' => 'baz' }
+  end
+  
+  it "makes http query parameters available as #query" do
+    @resource.query.to_h.should == { 'bar' => '7' }
+  end
+  
+  it "provides access to all derived parameters using #params" do
+    @resource.params.to_h.should == { 'foo' => 'baz', 'bar' => '7' }
   end
   
 end
