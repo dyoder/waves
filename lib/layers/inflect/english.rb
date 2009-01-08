91f75eccef7c56b74b267be756b09606ae32122b
Matthew King <automatthew@gmail.com>
2009-01-08 11:45:29 -0600

    legacy Resources::Paths#generate method removed

--- lib/layers/inflect/english.rb
+++ lib/layers/inflect/english.rb
@@ -22,38 +22,6 @@ module Waves
           Waves::Resources::Paths.module_eval do
             def resource ; self.class.resource.singular ; end
             def resources ; self.class.resource.plural ; end
-
-            # TODO: be nice to DRY this up ... basically the same code 
-            # as the mixin except with the singular / plural stuff
-            # mixed in ...
-            def generate( template, args )
-              return "/#{ args * '/' }" unless template.is_a?( Array ) and not template.empty?
-              path = []
-              ( "/#{ path * '/' }" ) if template.all? do | want |
-                case want
-                when true then path += args
-                when String then path << want
-                when Symbol
-                  case want
-                  when :resource then path << resource
-                  when :resources then path << resources
-                  else path << args.shift
-                  end
-                when Regexp then path << args.shift
-                when Hash
-                  key, value = want.to_a.first
-                  case key
-                  when :resource then path << resource
-                  when :resources then path << resources
-                  else 
-                    case value
-                    when true then path += args
-                    when String, Symbol, Regexp then path << args.unshift
-                    end
-                  end
-                end
-              end
-            end
           end
           
         end
