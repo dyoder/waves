15df4fe8530fb678303b50f4c97efe697fb87794
Krishna Krishnamaneni <kkrishnamaneni@ypcmc03347.yellowpages.local>
2009-01-21 17:19:23 -0800

    Configuration doesn't use the Rack DSL anymore, its reorganized so that rack middleware could be added to the rack application from any configuration file

--- lib/runtime/configuration.rb
+++ lib/runtime/configuration.rb
@@ -11,7 +11,7 @@ module Waves
 
       # Get the value of the given attribute. Typically, you wouldn't
       # use this directly.
-      def self.[]( name ) ; send "_#{name}" ; end
+      def self.[]( name ) ; send "_#{name}" rescue nil ; end
 
       # Define a new attribute. After calling this, you can get and set the value
       # using the attribute name as the method
@@ -22,7 +22,7 @@ module Waves
         end
         self[ name ] = nil
       end
-      
+
       def self.attributes( *names )
         names.each { |name| attribute( name ) }
       end
@@ -31,44 +31,53 @@ module Waves
 
     # The Default configuration defines sensible defaults for attributes required by Waves.
     class Default < Base
-      
+
       # define where a server should listen
       # can be overridden by -p and -h options
       attributes( :host, :port, :ports )
-      
+
       # which server to use, ex: Waves::Servers::Mongrel
       attribute( :server )
-      
+
       # where will the logger write to? can be a IO object or a pathname
       # also can set the level here to :fatal, :debug, :warn, :info
       attribute( :log )
-      
+
       # which modules are going to be reloaded on each request?
       attribute( :reloadable )
-      
+
       # which resource to use as the "main" resource for this server
-      attribute( :resource ) 
-      
+      attribute( :resource )
+
       # parameters for the database connection, varies by ORM
       attribute( :database )
-      
+
       # set the debug mode flag; typically done in dev / test configurations
       attribute( :debug )
-      
+
       # what object to use as the main Waves cache
       attribute( :cache )
-      
+
       # do you want to run a console thread (ex: LiveConsole)
       attribute( :console )
-      
+
       # are there any gems we need to check for on startup?
       attributes( :dependencies )
-      
+
       # Provides access to the Waves::MimeTypes class via the configuration. You
       # can override #mime_types to return your own MIME types repository class.
       def self.mime_types
         Waves::MimeTypes
       end
+
+      # Register rack middleware using this method.
+      #@registered_middleware = []
+      #def self.register_rack_middleware(&block)
+      #  @registered_middleware << block
+      #end
+      #def self.registered_rack_middleware
+      #  @registered_middleware
+      #end
 
       # Defines the application for use with Rack.  Treat this method like an
       # instance of Rack::Builder
@@ -76,25 +85,28 @@ module Waves
         if block_given?
           self['application'] = Rack::Builder.new( &block )
         else
-          self['application']
+          self['application'] ||= Rack::Builder.new
         end
       end
-      
+
       # default options
       debug true
       log :level => :info, :output => $stderr
       reloadable []
       dependencies []
       server Waves::Servers::WEBrick
-      application {
-        use ::Rack::ShowExceptions
-        use Rack::Session::Cookie, :key => 'rack.session',
-        # :domain => 'foo.com',
-        :path => '/',
-        :expire_after => 2592000,
-        :secret => 'Change it'
-        run ::Waves::Dispatchers::Default.new
-      }
+      #middleware = registered_rack_middleware
+
+        #require 'ruby-debug'; debugger
+        #middleware.each { |mw| puts "calling"; mw.call }
+
+
+
+      application.use ::Rack::ShowExceptions
+
+
+
+
     end
   end
 end
