5d2d09c3bdbb67ded4c36595b8b4997f8d25c5de
Roberto Gamboni <rgamboni@ypcmc03037.yellowpages.local>
2009-01-27 15:56:56 -0800

    updating for staying up with latest rack_builder branch

--- samples/blog/configurations/development.rb
+++ samples/blog/configurations/development.rb
@@ -1,3 +1,4 @@
+# require 'layers/rack/rack_cache'
 module Blog
 
   module Configurations
@@ -5,21 +6,18 @@ module Blog
     class Development < Default
 
       reloadable [ Blog ]
-      log :level => :debug
-      host '127.0.0.1'
-      port 4000
-
-      application do
-        use ::Rack::ShowExceptions
-        use ::Rack::Static, 
+      
+      # in case we want to use http caching
+      # include Waves::Cache::RackCache
+      application.use Rack::Session::Cookie, :key => 'rack.session',
+        # :domain => 'foo.com',
+        :path => '/',
+        :expire_after => 2592000,
+        :secret => 'Change it'
+      application.use ::Rack::Static, 
           :urls => %w( /css /javascript /favicon.ico ), 
           :root => 'public'
-        use Rack::Session::Cookie, 
-          :key => 'blog.session', :path => '/',
-          :expire_after => 3.days
-        run ::Waves::Dispatchers::Default.new
-      end
-      
+
       server Waves::Servers::Mongrel
 
     end
