d37d94d0c1a6aebca7b88c958078639936f8f781
Roberto Gamboni <rgamboni@ypcmc03037.yellowpages.local>
2009-01-27 15:56:56 -0800

    updating for staying up with latest rack_builder branch

--- samples/blog/configurations/production.rb
+++ samples/blog/configurations/production.rb
@@ -10,17 +10,6 @@ module Blog
       host '0.0.0.0'
       port 4000
 
-      application do
-        use ::Rack::Static, 
-          :urls => %w( /css /javascript /favicon.ico ), 
-          :root => 'public'
-        use Rack::Session::Cookie, 
-          :key => 'blog.session', :path => '/',
-          :expire_after => 3.days
-        run ::Waves::Dispatchers::Default.new
-      end
-      
-
     end
   end
 end
