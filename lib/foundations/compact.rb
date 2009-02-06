0a94c679cba2c435f139183c64f826dcb2ede0cf
Roberto Gamboni <rgamboni@ypcmc03037.yellowpages.local>
2009-02-05 17:28:17 -0800

    modified the templates to adopt the rack builder changes

--- lib/foundations/compact.rb
+++ lib/foundations/compact.rb
@@ -27,17 +27,14 @@ module Waves
               log :level => :error, :output => ( "log.#{$$}" ), :rotation => :weekly
               port 80
               host '0.0.0.0'
-              server Waves::Servers::Mongrel
-              application {
-                use Rack::Session::Cookie, :key => 'rack.session',
-                  # :domain => 'foo.com',
-                  :path => '/',
-                  :expire_after => 2592000,
-                  :secret => 'Change it'
-
-                run ::Waves::Dispatchers::Default.new
-              }
-              
+              #server Waves::Servers::Mongrel
+            
+              application.use Rack::Session::Cookie, 
+              	:key => 'rack.session',
+        		# :domain => 'foo.com',
+        		:path => '/',
+        		:expire_after => 2592000,
+        		:secret => 'Change it'
             })
           })
         }
