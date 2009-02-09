cca45da756b05cc7123dc32cbec9379aa3e928c5
Krishna Krishnamaneni <kkrishnamaneni@YPCMC03347.local>
2009-02-09 13:34:15 -0800

    Added ability to unregister waves workers

--- lib/runtime/tuplespace/manager.rb
+++ lib/runtime/tuplespace/manager.rb
@@ -3,15 +3,36 @@ require 'rinda/tuplespace'
 module Waves
   module TupleSpace
     class Manager
+      class Broker
+        def initialize(ts)
+          @ts = ts
+        end
+        def unregister(tuple)
+          puts('Unregistering....' + tuple.to_s )
+          @ts.take(tuple, 0) rescue nil
+        end
+        def run
+          loop do
+            puts "trying to look for unregisters..."
+            #Take unregister requests
+            tuple = @ts.take([:worker, nil, nil, :unregister])
+            unregister(tuple[0..2])
+          end
+        end
+      end
       def initialize
       end
       def run
         server = DRb.start_service
+        puts server
+            require 'ruby-debug'; debugger
         Rinda::RingServer.new Rinda::TupleSpace.new
         ts = Rinda::TupleSpace.new
         server = DRb.start_service
         provider = Rinda::RingProvider.new :TupleSpace, ts, 'Tuple Space'
         provider.provide
+        puts "Starting waves broker..."
+        Thread.new{ Broker.new(ts).run }
         puts "Started waves manager.."
         DRb.thread.join
       end
