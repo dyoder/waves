5ffd000fcde523aa2b94e3dbe11a6108d2d2e3d1
Roberto Gamboni <cproby2005@gmail.com>
2009-02-24 23:08:55 -0800

    removed default cache ttl and set request with cookies cacheable

--- lib/layers/rack/rack_cache.rb
+++ lib/layers/rack/rack_cache.rb
@@ -13,14 +13,14 @@ module Waves
             #set cache related options
             set :verbose,     true
             # default_ttl will be add to any cacheable response without explicit indication of max-age.
-            set :default_ttl, 60 * 60 * 24
+            # set :default_ttl, 60 * 60 * 24
             # store can be heap, memcache or disk. Default option is heap.
             #set :metastore,   'file:/var/cache/rack/meta'
             set :entitystore, 'file:./cache/rack/body'
             # request containing 'Authorization' and 'Cookie' headers are defined 'private' and thus not cacheable.
             # overriding the private_headers will define which headers make the request not cacheable.
             # instead of overriding this config, you may choose to use the header 'Vary' in your application.
-            # set :private_headers, ['Authorization']
+            set :private_headers, ['Authorization']
           end
         
       end
