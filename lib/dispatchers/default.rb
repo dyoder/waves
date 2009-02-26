465a4e089c44b3251d533f8930b3baf49d939d0d
Krishna Krishnamaneni <kkrishnamaneni@attinteractive.com>
2009-02-25 17:09:54 -0800

    Created matcher for :ext, changed the default response header to either ext or 'text/html' so that we won't be dependant on accept headers from the browsers.

--- lib/dispatchers/default.rb
+++ lib/dispatchers/default.rb
@@ -4,10 +4,10 @@ module Waves
 
     class Default < Base
 
-      # Takes a Waves::Request and returns a Waves::Response    
+      # Takes a Waves::Request and returns a Waves::Response
       def safe( request  )
         # set a default content type -- this can be overridden by the resource
-        request.response.content_type = request.accept.default
+        request.response.content_type = request.ext || 'text/html'
         resource = Waves.config.resource.new( request )
         if request.response.body.empty?
           request.response.write resource.process.to_s
